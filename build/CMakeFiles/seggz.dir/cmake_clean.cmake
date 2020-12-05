1fa53b319533fd013b50fd1f646cb5eaa85af6b0
tailow <revitate@hotmail.com>
2020-12-05 21:29:51 +0200

    init 2

---
+++ build/CMakeFiles/seggz.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/seggz.dir/src/main.cpp.obj"
+  "libseggz.dll.a"
+  "seggz.exe"
+  "seggz.exe.manifest"
+  "seggz.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/seggz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int, char **)
+{
+    std::cout << "Hello, world!\n";
+
+    int yo;
+    std::cin >> yo;
+}
