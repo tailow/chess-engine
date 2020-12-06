f73cd261bbcab110acda9e57224efcdbc5ecbd54
DAN-KOTI\Daniel <gg@gmail.com>
2020-12-06 21:06:51 +0200

    evavl????

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1,15 +1,70 @@
 #include "../lib/thc/thc.h"
 using namespace std;
-int evaluate(thc::ChessRules board)
+double evaluate(thc::ChessRules board)
 {
-    int evaluation;
-    vector <thc::Move> legalMoves;
-    vector <thc::Move> goodmoves;
+    double evaluation;
+    double score;
+    double bscore;
 
-    board.GenLegalMoveList(legalMoves);
 
-    board.white()
+    for( square=a8; square<=h1; ++square )
+    {
 
+    char piece=squares[square];
+
+    if( (white&&IsWhite(piece)) || (!white&&IsBlack(piece)) )
+    {
+        switch( piece )
+            {
+                case 'P':
+                {
+                    score = score + 1
+                    break;
+                }
+                case 'p':
+                {
+                    bscore = bscore + 1
+                    break;
+                }
+                case 'N':
+                {
+                    score = score + 3
+                }
+                case 'n':
+                {
+                    bscore = bscore + 3
+                }
+                case 'B':
+                {
+                    score = score + 3.5
+                }
+                case 'b':
+                {
+                    bscore = bscore + 3.5
+                    break;
+                }
+                case 'R':
+                {
+                    score = score + 5
+                }
+                case 'r':
+                {
+                    bscore = bscore + 5
+                    break;
+                }
+                case 'Q':
+                {
+                    score = score + 9
+                }
+                case 'q':
+                {
+                    bscore = bscore + 9
+                    break;
+                }
+        }
+    }
+
+    evaluation = score - bscore;
     
     return evaluation;
 }
