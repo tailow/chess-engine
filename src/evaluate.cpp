7fba25dc65fd56e8ac123c279803bd427c1bc04b
DAN-KOTI\Daniel <gg@gmail.com>
2020-12-07 16:34:01 +0200

    duplicate removal

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -100,17 +100,6 @@ double evaluate(thc::ChessRules board)
         }
     }
     
-    // prefer big center (does not really work either)
-    if (board.squares[28] == 'P' && board.squares[29] == 'P')
-    {
-        evaluation += 0.5;
-    }
-
-    if (board.squares[36] == 'p' && board.squares[37] == 'p')
-    {
-        evaluation -= 0.5;
-    }
-
     // try to castle does not work somereason test fen (r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 4 4)
     if (board.squares[58] == 'K' || board.squares[62] == 'K')
     {
