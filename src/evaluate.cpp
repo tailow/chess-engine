04029101ddb19e727603f85d977e78f21600d6b0
tailow <revitate@hotmail.com>
2020-12-09 16:04:13 +0200

    checkmate optimization

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -7,12 +7,10 @@ double evaluate(thc::ChessRules board)
 {
     volatile double evaluation = 0;
 
-    vector<thc::Move> legalMoves;
     // Queens alive?
     bool wkingSafety = true;
     bool bkingSafety = true;
 
-    board.GenLegalMoveList(legalMoves);
     for (volatile int rank = 0; rank < 8; rank++)
     {
         for (volatile int file = 0; file < 8; file++)
@@ -22,29 +20,29 @@ double evaluate(thc::ChessRules board)
             case 'P':
             {
                 evaluation += 1;
-                /*
+
                 if (rank < 5 && (file == 4 || file == 3 || file == 2))
                 {
                     evaluation += 0.33;
                 }
-                */
+
                 break;
             }
             case 'p':
             {
                 evaluation -= 1;
-                /*
+
                 if (rank > 2 && (file == 4 || file == 3 || file == 2))
                 {
                     evaluation -= 0.33;
                 }
-                */
+
                 break;
             }
             case 'N':
             {
                 evaluation += 3;
-                /*
+
                 if (rank == 7)
                 {
                     evaluation -= 0.33;
@@ -53,13 +51,13 @@ double evaluate(thc::ChessRules board)
                 {
                     evaluation -= 0.5;
                 }
-                */
+
                 break;
             }
             case 'n':
             {
                 evaluation -= 3;
-                /*
+
                 if (rank == 0)
                 {
                     evaluation += 0.33;
@@ -68,67 +66,67 @@ double evaluate(thc::ChessRules board)
                 {
                     evaluation += 0.5;
                 }
-                */
+
                 break;
             }
             case 'B':
             {
                 evaluation += 3.2;
-                /*
+
                 if (rank == 7)
                 {
                     evaluation -= 0.2;
                 }
-                if(rank==6 && (file==1 ||file == 6))
+                if (rank == 6 && (file == 1 || file == 6))
                 {
                     evaluation += 0.35;
                 }
-                */
+
                 break;
             }
             case 'b':
             {
                 evaluation -= 3.2;
-                /*
+
                 if (rank == 0)
                 {
                     evaluation += 0.2;
                 }
-                if(rank==1 && (file==1 ||file == 6))
+                if (rank == 1 && (file == 1 || file == 6))
                 {
                     evaluation -= 0.35;
                 }
-                */
+
                 break;
             }
             case 'R':
             {
                 evaluation += 5;
-                /*
-                if(file == 0 ||file == 1 ||file ==6 || file ==7)
+
+                if (file == 0 || file == 1 || file == 6 || file == 7)
                 {
                     evaluation -= 0.8;
                 }
-                if(rank == 0 || rank == 1 || rank == 6 || rank==7)
+                if (rank == 0 || rank == 1 || rank == 6 || rank == 7)
                 {
-                    evaluation +=0.22;
+                    evaluation += 0.22;
                 }
-                */
+
                 break;
             }
             case 'r':
             {
                 evaluation -= 5;
-                /*
-                if(file == 0 ||file == 1 ||file ==6 || file ==7)
+
+                if (file == 0 || file == 1 || file == 6 || file == 7)
                 {
-                    evaluation +=0.8;
+                    evaluation += 0.8;
                 }
-                if(rank == 0 || rank == 1 || rank == 6 || rank==7)
+                if (rank == 0 || rank == 1 || rank == 6 || rank == 7)
                 {
-                    evaluation -=0.22;
+                    evaluation -= 0.22;
                 }
-                */
+
                 break;
             }
             case 'Q':
@@ -139,14 +137,14 @@ double evaluate(thc::ChessRules board)
             }
             case 'q':
             {
-                evaluation -= 9;                
-               // wkingSafety = false;
+                evaluation -= 9;
+                // wkingSafety = false;
                 break;
             }
             }
         }
     }
-/*
+
     // try to castle does not work somereason test fen (r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 4 4)
     if ((board.squares[58] == 'K' || board.squares[62] == 'K') && wkingSafety == false)
     {
@@ -158,25 +156,37 @@ double evaluate(thc::ChessRules board)
     {
         evaluation -= 0.6;
     }
-*/
+
     //checkmate
-    if (board.WhiteToPlay() && board.AttackedSquare(board.wking_square, false) && legalMoves.size() == 0)
+    if (board.WhiteToPlay() && board.AttackedSquare(board.wking_square, false))
     {
-        evaluation -= 1000000;
+        vector<thc::Move> legalMoves;
+        board.GenLegalMoveList(legalMoves);
+
+        if (legalMoves.size() == 0)
+        {
+            evaluation -= 1000000;
+        }
     }
 
-    else if (!board.WhiteToPlay() && board.AttackedSquare(board.bking_square, true) && legalMoves.size() == 0)
+    else if (!board.WhiteToPlay() && board.AttackedSquare(board.bking_square, true))
     {
-        evaluation += 1000000;
+        vector<thc::Move> legalMoves;
+        board.GenLegalMoveList(legalMoves);
+
+        if (legalMoves.size() == 0)
+        {
+            evaluation += 1000000;
+        }
     }
 
     //stalemate
-    else if (legalMoves.size() == 0)
+    else if (board.full_move_count)
     {
         evaluation = 0;
     }
 
-    evaluation += ((rand() % 2) -1);
+    evaluation += ((rand() % 2) - 1);
 
     //cout << wkingSafety << "  <-w  b-->  " << bkingSafety;
 
